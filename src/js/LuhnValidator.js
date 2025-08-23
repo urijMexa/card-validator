e27718ad6b2f43606e3f92017c33cdf6a114fc03
Yuri Blinnikov <blinnikovu5@gmail.com>
2025-08-23 14:30:30 +0800

    fix33

--- src/js/LuhnValidator.js
+++ src/js/LuhnValidator.js
@@ -1,9 +1,11 @@
 export default function isValid(cardNumber) {
-    if (!/^\d+$/.test(cardNumber)) {
+    const cleanedCardNumber = cardNumber.replace(/\D/g, '');
+
+    if (!/^\d+$/.test(cleanedCardNumber)) {
         return false;
     }
 
-    const digits = cardNumber.split('').map(Number);
+    const digits = cleanedCardNumber.split('').map(Number);
     let sum = 0;
 
     for (let i = digits.length - 1; i >= 0; i -= 1) {
--- src/js/LuhnValidator.test.js
+++ src/js/LuhnValidator.test.js
@@ -7,3 +7,11 @@ test('should validate correct card number', () => {
 test('should invalidate incorrect card number', () => {
     expect(isValid('4561-2612-1234-1235')).toBeFalsy();
 });
+
+test('should work with spaces', () => {
+    expect(isValid('4561 2612 1234 1234')).toBeTruthy();
+});
+
+test('should return false for empty string', () => {
+    expect(isValid('')).toBeFalsy();
+});
